36f3889c627fb8797ef4fefd3f3b2b56696e1b55
ANKUSH RAMRAO ADE <49829045+adeankush3@users.noreply.github.com>
2022-02-01 17:42:53 +0530

    Update Program.cs

--- Day06_Logical_Program/Day06_Logical_Program/Program.cs
+++ Day06_Logical_Program/Day06_Logical_Program/Program.cs
@@ -1,15 +1,72 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day06_Logical_Program
+namespace LogicalPrograms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Logical Programs\n");
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("****************");
+                Console.WriteLine("Enter\n0 for Exit\n" +
+               "1 for Fibonacci Series \n" +
+               "2 for Perfect Number\n" +
+               "3 for Prime Number\n" +
+               "4 for Reverse Number\n" +
+               "5 for Coupon Generation\n" +
+               "6 for Stop Watch\n");
+
+                Console.WriteLine("****************\n");
+
+                Console.WriteLine("Enter your option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (option)
+                {
+                    case 0:
+                        flag = false;
+                        break;
+
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.GenerateFibonacciSeries();
+                        break;
+
+                    case 2:
+                        PerfectNumber perfectNumber = new PerfectNumber();
+                        perfectNumber.isPerfectNumber();
+                        break;
+
+                    case 3:
+                        PrimeNumber primeNumber = new PrimeNumber();
+                        primeNumber.isPrime();
+                        break;
+
+                    case 4:
+                        ReverseNumber reverseNumber = new ReverseNumber();
+                        reverseNumber.ReturnReverse();
+                        break;
+
+                    case 5:
+                        CouponGeneration coupon = new CouponGeneration();
+                        coupon.GenerateCoupon();
+                        break;
+
+                    case 6:
+                        StopWatch watch = new StopWatch();
+                        watch.SimulateStopWatch();
+                        break;
+                }
+            }
         }
     }
 }
